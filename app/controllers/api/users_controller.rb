f8fb8c67e1b590f953e8165a49227444e78753ee
Remington <remingtonnberg@gmail.com>
2020-11-11 14:38:39 -0600

    adds users show

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,4 +12,14 @@ class Api::UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def show
+    if current_user
+      @user = current_user
+
+      render "show.json.jb"
+    else
+      render json: { message: "Nope." }
+    end
+  end
 end
